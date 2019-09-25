422072fb9c0d8a9f5d370b6c69d0666966cca43c
Grifiun <denii.grifon@gmail.com>
2019-09-25 01:00:20 -0600

    Login inicial

---
+++ src/main/webapp/script-login.js
@@ -0,0 +1,13 @@
+var username = document.getElementById("username");
+var contra1 = document.getElementById("pass1");
+var contra2 = document.getElementById("pass2");
+
+function iniciarSesion(){    
+    if(username == "" ){
+        alert("Faltan datos por rellenar"); 
+    }
+    else{        
+        alert("Entraste con exito crack");      
+    }
+    
+}
